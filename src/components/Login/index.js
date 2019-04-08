51fffee8936db141245b80b163f8fa86f3eb797c
josecleiton <jcleitonbc@gmail.com>
2019-04-07 22:27:56 -0300

    css milionarios

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,1 +1,7 @@
 // AQUI FICARÁ O CAMPO DE LOGIN
+import React, { Component } from "react";
+export default class Login extends Component {
+  render() {
+    return <span>LOGIN_COMPONENT_LOADED</span>;
+  }
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Login from "../../components/Login";
+//import ReactDOM from "react-dom";
 import "./style.css";
 import logo from "../../logo.png";
 
@@ -7,25 +9,56 @@ export default class Main extends Component {
     return (
       <div id="main-page">
         <header id="bar">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. */}
+          <div id="tecnicos">
+            <i class="fas fa-info-circle" />
+            {/*
+              DIV PARA MOSTRAR STATUS:
+                TECNICOS ONLINE OU HORARIO DE FUNCIONAMENTO
+            */}
+            Técnicos online: {}
+          </div>
+          <div id="area-tecnico">
+            <i class="fas fa-user" />
+            Área dos Técnicos
+            <Login />
+          </div>
         </header>
         <div id="container">
           <div id="logo">
             <img src={logo} alt="" className="App-logo" />
           </div>
 
-          <form action="" method="get" className="checar-chamado">
+          <form className="checar-chamado">
             <input
               placeholder="Insira seu email ou o número do chamado..."
-              type="text"
-              name=""
+              name="input"
               id=""
             />
-            <button type="submit"> Verificar </button>
           </form>
+          <div className="error">
+            Digite um email valido ou um número de chamado!
+          </div>
         </div>
       </div>
     );
   }
+  formChecker = () => {
+    let inputVal = document
+      .querySelector("input[name=input]")
+      .value.toLowerCase();
+    let errorEl = document.getElementsByClassName("error")[0];
+    if (inputVal.indexOf("@") === -1) {
+      let numberRegex = /[0-9]/;
+      if (!numberRegex.test(inputVal) || inputVal.length > 6) {
+        errorEl.style.display = "flex";
+        errorEl.style.justifyContent = "center";
+        errorEl.style.justifyItems = "center";
+        errorEl.style.margin = "auto";
+      }
+    } else {
+      errorEl.style.display = "none";
+    }
+    // TESTAR NO SERVER SE É UM EMAIL VALIDO
+  };
 }
