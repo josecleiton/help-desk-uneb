a2cfae2dddf01f263e36f85c85361624964e64f1
josecleiton <jcleitonbc@gmail.com>
2019-04-08 18:00:52 -0300

    login area

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,34 @@
 // AQUI FICARÁ O CAMPO DE LOGIN
 import React, { Component } from "react";
+import "./style.css";
 export default class Login extends Component {
   render() {
-    return <span>LOGIN_COMPONENT_LOADED</span>;
+    return (
+      <div id="login">
+        <form action="">
+          <strong>Área de Login</strong>
+          <a href="#login" title="Área de acesso para técnicos.">
+            <i class="fas fa-user-tie" />
+          </a>
+          <br />
+          <input
+            type="text"
+            name="login-lg"
+            id=""
+            placeholder="Usuário"
+            required
+          />
+          <input type="password" name="login-pw" id="" required />
+          <nav>
+            <button type="submit" className="login-button">
+              Entrar
+            </button>
+            <a href="http://google.com" className="esqueceu">
+              Esqueceu a senha?
+            </a>
+          </nav>
+        </form>
+      </div>
+    );
   }
 }
