992026d985a5a9b1e4c66613f2ab2f1c19812299
Danilo Nascimento <danilodelete@hotmail.com>
2019-05-20 21:27:54 -0300

    Form específico do Setor TI

---
+++ src/components/AbrirChamado/FormTi/index.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+
+const FormTi = () => (
+  <Fragment>
+    <div>
+      <small>
+        <i className="fas fa-cogs" />
+        Software:
+      </small>
+      <input type="text" />
+    </div>
+    <div>
+      <small>
+        <i className="fas fa-calendar-day" />
+        data de Utilização:
+      </small>
+      <input type="date" />
+      <small>
+        <i className="fas fa-network-wired" />
+        Sala:
+      </small>
+      <select>
+        <option value="">Informe o laboratório</option>
+        <option value="">LAMI I</option>
+        <option value="">LAMI II</option>
+      </select>
+    </div>
+    <div>
+      <small>
+        <i className="fas fa-globe-europe" />
+        Link para download:
+      </small>
+      <input type="text" />
+    </div>
+    <div>
+      <small>
+        <i className="fas fa-plus-square" />
+        plug-ins relacionados:
+      </small>
+      <input type="text" />
+    </div>
+  </Fragment>
+);
+export default FormTi;
