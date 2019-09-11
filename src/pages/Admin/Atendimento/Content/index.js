fd1febbed6a6b8674b32c41b27b9e92de4507276
Danilo Nascimento <danilodelete@hotmail.com>
2019-09-11 20:20:19 -0300

    bug qualidade

--- src/pages/Admin/Atendimento/Content/index.js
+++ src/pages/Admin/Atendimento/Content/index.js
@@ -1,47 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import HistoricoMovimentacao from '../../../../components/HistoricoChamado';
+import api from '../../../../services/api';
 
-const Content = (props) => {
-  const { id } = props;
-  return (
-    <div className="admin-atendimento-content">
-      <h2>
-Número do Chamado: #
-        {id}
-      </h2>
-      <p>
-        <strong>Solicitante:</strong>
-        {' '}
+class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      information: null,
+    };
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    api
+      .post('/api/chamado/called', { id })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log(window.location.href);
+    const { information } = this.state;
+    console.log(information);
+    return (
+      <div className="admin-atendimento-content">
+        <h2>Número do Chamado: #</h2>
+        <p>
+          <strong>Solicitante:</strong>
+          {' '}
 user@email.com
-      </p>
-      <p>
-        <strong>Status:</strong>
-        {' '}
+        </p>
+        <p>
+          <strong>Status:</strong>
+          {' '}
 Aberto
-      </p>
-      <p>
-        <strong>Data de Abertura:</strong>
-        {' '}
+        </p>
+        <p>
+          <strong>Data de Abertura:</strong>
+          {' '}
 dd/mm/YYYY
-      </p>
+        </p>
 
-      <p>
-        <strong>Área:</strong>
-        {' '}
+        <p>
+          <strong>Área:</strong>
+          {' '}
 TI
-      </p>
+        </p>
 
-      <p>
-        <strong>Anexo:</strong>
-        {' '}
+        <p>
+          <strong>Anexo:</strong>
+          {' '}
 Ver imagem
-      </p>
-      <HistoricoMovimentacao>Tabela de modificações vem aqui</HistoricoMovimentacao>
-    </div>
-  );
-};
+        </p>
+        <HistoricoMovimentacao>Tabela de modificações vem aqui</HistoricoMovimentacao>
+      </div>
+    );
+  }
+}
 Content.propTypes = {
   id: PropTypes.string.isRequired,
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 export default Content;
