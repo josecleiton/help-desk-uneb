16d5171813a2bb846c601e6e3be64aaebb4fd3b0
Daanilo-s19 <38538568+Daanilo-s19@users.noreply.github.com>
2019-11-27 11:40:06 -0300

    fix: baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://hduneb.000webhostapp.com/api"
 });
 
 export default api;
